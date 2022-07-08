cf15380a42e8b8115ed0d054c89fc986f797a66e
mehranAsghari <mhr.as1317@gmail.com>
2022-07-08 23:51:03 +0430

    add 4-print-learn-python-with-version-by-identifier.py
    print a sentece with to value.

---
+++ 4-print-learn-python-with-version-by-identifier.py
@@ -0,0 +1,6 @@
+subject = 'Python'
+version = '3.8'
+
+print("I am learning " + subject + " version " + version)
+print(f"I am learning {subject} version {version}")
+print("I am learning %s version %s"%(subject, version))
